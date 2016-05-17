6803eb3f9b2c8b7765fd9e315bf790c4f8645347
Ahmed-Mosharafa <ahmedmosharafa@gmail.com>
2016-05-17 17:29:24 +0200

    Adding Modularity to Newton Interpolation

--- Interpolation.cpp
+++ Interpolation.cpp
@@ -15,6 +15,15 @@ Interpolation::~Interpolation()
 {
     //dtor
 }
+////////////// Spline Interpolation ////////////////////////
+
+double Interpolation::findingInterval(double x[], double a, int n) {
+		for (int i = n-1; i > 0; i--){
+			if (a < x[i] && a > x[i-1])	{
+				return i-1;	
+			}
+		} 
+}
 
 double Interpolation::Spline(double x[], double y[], double a, int n){
 	int interval, i;
@@ -58,45 +67,58 @@ double Interpolation::Spline(double x[], double y[], double a, int n){
 		cj = c[interval];
 		dj = d[interval];
 		interpolant = aj + bj + pow(  cj * (a - x[interval]) , 2) + pow( dj * (a - x[interval]) , 3);
-		//cout << "\naj = " << aj << "\nbj = " << bj << "\ncj = " << cj << "\ndj = " << dj << "\n" << "x[interval] = " << x[interval] << "\n	" << "a: " << a << "\n";
+		cout << "\naj = " << aj << "\nbj = " << bj << "\ncj = " << cj << "\ndj = " << dj << "\n" << "x[interval] = " << x[interval] << "\n	" << "a: " << a << "\n";
 		return interpolant;
 	}
 }
-double Interpolation::findingInterval(double x[], double a, int n) {
-		for (int i = n-1; i > 0; i--){
-			if (a < x[i] && a > x[i-1])	{
-				return i-1;	
-			}
-		} 
-}
-double Interpolation::NewtInt(double x[], double y[], double a, int n)
-{
-    double fdd[n];   //initializng an array of Fdds 
-	double sum,mult;
-	int i,j;
-    //initializing FDDs with Ys 
-    for (i=0; i<n; i++){
-        fdd[i] = y[i];
-    }
-    //calculating FDDs for each iteration 
-    for (j=0; j<n-1; j++){
-        for (i=n-1; i>j;i--){	
-            	fdd[i] = ( fdd[i]-fdd[i-1] )/ ( x[i]-x[i-j-1]) ;
-        }
-    }
-    //evaluating fn(x) while calculating coefficients 
-    for(i=n-1;i>=0;i--)
+
+double Interpolation::evaluatingNewtonPoint(double a, double fdd[], int n, double x[]){
+	double sum, mult;
+	for(int i=n-1;i>=0;i--)
     {
         mult = 1;
-        for(j=0;j<i;j++){
+        for(int j=0;j<i;j++){
             mult *= (a-x[j]);
         }
         mult *= fdd[i];
         sum  += mult;
-        cout << "Iteration " << (n-1)-i << ": " << sum << "\n";
+    cout << "Iteration " << (n-1)-i << ": " << sum << "\n";
     }
     return sum;
 }
+double* Interpolation::calculateFdds(double x[], double y[], int n){
+    double fdd[n];
+    //initializing FDDs with Ys 
+    for (int i=0; i<n; i++){
+        fdd[i] = y[i];
+    }
+    //calculate Fdds 
+	for (int j=0; j<n-1; j++){
+        for (int i=n-1; i>j;i--){	
+            	fdd[i] = ( fdd[i]-fdd[i-1] )/ ( x[i]-x[i-j-1]) ;
+        }
+    }
+    return fdd;
+}
+double Interpolation::NewtInt(double x[], double y[], double a, int n)
+{
+    double *fdd;   //initializng an array of Fdds 
+	double interpolant;
+	int i,j;
+	
+    //calculating FDDs for each iteration 
+	fdd = calculateFdds(x, y , n);
+	
+    //evaluating fn(x) while calculating coefficients 
+    interpolant = evaluatingNewtonPoint(a, fdd, n, x);
+    return interpolant;
+}
+
+/**
+	@param a[] array to be printed
+	@param n   size of the array
+	helper function for printing the array
+*/
 void printArray(double a[], int n){
 	for (int i = 0; i<n; i++){
 		cout << a[i] << " ";
@@ -108,8 +130,8 @@ int main(){
     double newton_values[] = {0.0,1.3862944,1.7917595,1.6094379,1.0986123,0.4054641,0.9162907}; //1.2527630
     double spline_test[]   = {3,4.5,7};
 	double spline_values[] = {2.5,1,2.5};
-    double newton_point    = 3.5;
 	double spline_point    = 5;	
+    double newton_point    = 3.5;
 	Interpolation interpolate ;  
 	cout << "Test Data of Newton Interpolation\n"; 
 	printArray(newton_test, 7);
@@ -121,4 +143,3 @@ int main(){
 	cout << "\nSpline Interpolation of point " << spline_point << " is: " << interpolate.Spline(spline_test, spline_values, spline_point ,3);
 	return 0;
 }
-
